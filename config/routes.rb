39fb0c1d5119f39b93203ea280b38252e1e8aa63
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-01 13:26:17 +0900

    add route for messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :channels
+  resources :channels do
+    resources :messages
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
